35f9e0b054889e54d7572ea32f937e65353ac348
pontus <pontus_nystrom_@hotmail.com>
2025-07-12 11:33:31 +0200

    Start

--- js/quiz-data.js
+++ js/quiz-data.js
@@ -1,13 +1,13 @@
 const quizData = {
     "svenska-faglar": [
         {
-            image: "bilder/faglar/koltrast.jpg",
+            image: "bilder/faglar/koltrast.png",
             audio: "ljud/fagelsang/koltrast.mp3",
             swedishName: "Koltrast",
             latinName: "Turdus merula"
         },
         {
-            image: "bilder/faglar/bofink.jpg",
+            image: "bilder/faglar/bofink.png",
             audio: "ljud/fagelsang/bofink.mp3",
             swedishName: "Bofink",
             latinName: "Fringilla coelebs"
@@ -15,213 +15,232 @@ const quizData = {
     ],
     "svenska-daggdjur": [
         {
-            image: "bilder/daggdjur/bjorn.jpg",
+            image: "bilder/daggdjur/bjorn.png",
             swedishName: "Brunbjörn",
             latinName: "Ursus arctos"
         },
         {
-            image: "bilder/daggdjur/varg.jpg",
+            image: "bilder/daggdjur/varg.png",
             swedishName: "Varg",
             latinName: "Canis lupus"
         }
     ],
     "vara-blommor": [
         {
-            image: "bilder/vaxter/bla-sippa.jpg",
+            image: "bilder/vaxter/bla-sippa.png",
             swedishName: "Blåsippa",
             latinName: "Hepatica nobilis"
         },
         {
-            image: "bilder/vaxter/vit-sippa.jpg",
+            image: "bilder/vaxter/vit-sippa.png",
             swedishName: "Vitsippa",
             latinName: "Anemone nemorosa"
         }
     ],
     "svenska-fiskar": [
         {
-            image: "bilder/fiskar/al.jpg",
+            image: "bilder/fiskar/al.png",
             swedishName: "Ål",
             latinName: "Anguilla anguilla"
         },
         {
-            image: "bilder/fiskar/karp.jpg",
+            image: "bilder/fiskar/karp.png",
             swedishName: "Karp",
             latinName: "Cyprinus carpius"
         },
         {
-            image: "bilder/fiskar/ruda.jpg",
+            image: "bilder/fiskar/ruda.png",
             swedishName: "Ruda",
             latinName: "Carassius carassius"
         },
         {
-            image: "bilder/fiskar/sutare.jpg",
+            image: "bilder/fiskar/sutare.png",
             swedishName: "Sutare",
             latinName: "Tinca tinca"
         },
         {
-            image: "bilder/fiskar/asp.jpg",
+            image: "bilder/fiskar/asp.png",
             swedishName: "Asp",
             latinName: "Aspius aspius"
         },
         {
-            image: "bilder/fiskar/loja.jpg",
+            image: "bilder/fiskar/loja.png",
             swedishName: "Löja (benlöja)",
             latinName: "Alburnus alburnus"
         },
         {
-            image: "bilder/fiskar/brax.jpg",
+            image: "bilder/fiskar/brax.png",
             swedishName: "Brax",
             latinName: "Abramis brama"
         },
         {
-            image: "bilder/fiskar/bjorkna.jpg",
+            image: "bilder/fiskar/bjorkna.png",
             swedishName: "Björkna",
             latinName: "Blicca bjoerkna"
         },
         {
-            image: "bilder/fiskar/vimma.jpg",
+            image: "bilder/fiskar/vimma.png",
             swedishName: "Vimma",
             latinName: "Vimba vimba"
         },
         {
-            image: "bilder/fiskar/elritsa.jpg",
+            image: "bilder/fiskar/elritsa.png",
             swedishName: "Elritsa",
             latinName: "Phoxinus phoxinus"
         },
         {
-            image: "bilder/fiskar/sarv.jpg",
+            image: "bilder/fiskar/sarv.png",
             swedishName: "Sarv",
             latinName: "Scardinius erythophtalmus"
         },
         {
-            image: "bilder/fiskar/mort.jpg",
+            image: "bilder/fiskar/mort.png",
             swedishName: "Mört",
             latinName: "Rutilus rutilus"
         },
         {
-            image: "bilder/fiskar/farna.jpg",
+            image: "bilder/fiskar/farna.png",
             swedishName: "Färna",
             latinName: "Leuciscus cephalus"
         },
         {
-            image: "bilder/fiskar/id.jpg",
+            image: "bilder/fiskar/id.png",
             swedishName: "Id",
             latinName: "Leuciscus idus"
         },
         {
-            image: "bilder/fiskar/nissoga.jpg",
+            image: "bilder/fiskar/nissoga.png",
             swedishName: "Nissöga",
             latinName: "Cobitis taenia"
         },
         {
-            image: "bilder/fiskar/gronling.jpg",
+            image: "bilder/fiskar/gronling.png",
             swedishName: "Grönling",
             latinName: "Barbatula barbatula"
         },
         {
-            image: "bilder/fiskar/mal.jpg",
+            image: "bilder/fiskar/mal.png",
             swedishName: "Mal",
             latinName: "Silurus glanis"
         },
         {
-            image: "bilder/fiskar/gadda.jpg",
+            image: "bilder/fiskar/gadda.png",
             swedishName: "Gädda",
             latinName: "Esox lucius"
         },
         {
-            image: "bilder/fiskar/lax.jpg",
+            image: "bilder/fiskar/lax.png",
             swedishName: "Lax",
             latinName: "Salmo salar"
         },
         {
-            image: "bilder/fiskar/oring.jpg",
+            image: "bilder/fiskar/oring.png",
             swedishName: "Öring",
             latinName: "Salmo trutta"
         },
         {
-            image: "bilder/fiskar/storroding.jpg",
+            image: "bilder/fiskar/storroding.png",
             swedishName: "Storröding",
             latinName: "Salvelinus umbla"
         },
         {
-            image: "bilder/fiskar/storrefjallroding.jpg",
+            image: "bilder/fiskar/storrefjallroding.png",
             swedishName: "Större fjällröding",
             latinName: "Salvelinus alpinus"
         },
         {
-            image: "bilder/fiskar/harr.jpg",
+            image: "bilder/fiskar/harr.png",
             swedishName: "Harr",
             latinName: "Thymallus thymallus"
         },
         {
-            image: "bilder/fiskar/regnbage.jpg",
+            image: "bilder/fiskar/regnbage.png",
             swedishName: "Regnbåge",
             latinName: "Oncorhynchus mykiss"
         },
         {
-            image: "bilder/fiskar/sik.jpg",
+            image: "bilder/fiskar/sik.png",
             swedishName: "Sik",
             latinName: "Coregonus lavaretus"
         },
         {
-            image: "bilder/fiskar/sikloja.jpg",
+            image: "bilder/fiskar/sikloja.png",
             swedishName: "Siklöja",
             latinName: "Coregonus albula"
         },
         {
-            image: "bilder/fiskar/nors.jpg",
+            image: "bilder/fiskar/nors.png",
             swedishName: "Nors",
             latinName: "Osmerus eperlanus"
         },
         {
-            image: "bilder/fiskar/lake.jpg",
+            image: "bilder/fiskar/lake.png",
             swedishName: "Lake",
             latinName: "Lota lota"
         },
         {
-            image: "bilder/fiskar/storspigg.jpg",
+            image: "bilder/fiskar/storspigg.png",
             swedishName: "Storspigg",
             latinName: "Gasterosteus aculeatus"
         },
         {
-            image: "bilder/fiskar/smaspigg.jpg",
+            image: "bilder/fiskar/smaspigg.png",
             swedishName: "Småspigg",
             latinName: "Pungitius pungitius"
         },
         {
-            image: "bilder/fiskar/stensimpa.jpg",
+            image: "bilder/fiskar/stensimpa.png",
             swedishName: "Stensimpa",
             latinName: "Cottus gobio"
         },
         {
-            image: "bilder/fiskar/rysksimpa.jpg",
+            image: "bilder/fiskar/rysksimpa.png",
             swedishName: "Rysk simpa",
             latinName: "Cottus koshewnikowi"
         },
         {
-            image: "bilder/fiskar/bergsimpa.jpg",
+            image: "bilder/fiskar/bergsimpa.png",
             swedishName: "Bergsimpa",
             latinName: "Cottus poecilopus"
         },
         {
-            image: "bilder/fiskar/hornsimpa.jpg",
+            image: "bilder/fiskar/hornsimpa.png",
             swedishName: "Hornsimpa",
             latinName: "Triglopsis quadricornis"
         },
         {
-            image: "bilder/fiskar/abborre.jpg",
+            image: "bilder/fiskar/abborre.png",
             swedishName: "Abborre",
             latinName: "Perca fluviatilis"
         },
         {
-            image: "bilder/fiskar/gos.jpg",
+            image: "bilder/fiskar/gos.png",
             swedishName: "Gös",
             latinName: "Stizostedion lucioperca"
         },
         {
-            image: "bilder/fiskar/gars.jpg",
+            image: "bilder/fiskar/gars.png",
             swedishName: "Gärs",
             latinName: "Gymnocephalus cernuus"
+        },
+                {
+            image: "bilder/fiskar/Havsnejonoga.png",
+            swedishName: "Havsnejonöga",
+            latinName: "Petromyzon marinus"
+        },
+                {
+            image: "bilder/fiskar/Flodnejonoga.png",
+            swedishName: "Flodnejonöga",
+            latinName: "Lampetra fluviatilisa"
+        },
+                {
+            image: "bilder/fiskar/Bäcknejonoga.png",
+            swedishName: "Bäcknejonöga",
+            latinName: "Lampetra planeri"
+        },
+                    image: "bilder/fiskar/gos.png",
+            swedishName: "Störfiskar",
+            latinName: "Acipenseridae"
         }
     ]
 };
